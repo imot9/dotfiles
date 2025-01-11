f7762bcc1fc8095573b304612ad7f6d6c2a11d23
Tamas Szamel <szamel.tamas9@gmail.com>
2025-01-11 08:54:29 +0100

    rebind vim navigation

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,3 +1,23 @@
+# im sorry
+vim.api.nvim_set_keymap('n', 'j', 'h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'k', 'j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'l', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';', 'l', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v', 'j', 'h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'k', 'j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'l', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', ';', 'l', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('o', 'j', 'h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('o', 'k', 'j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('o', 'l', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('o', ';', 'l', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', 'h', '<nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'h', '<nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('o', 'h', '<nop>', { noremap = true, silent = true })
+
 vim.g.mapleader = " "
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -5,6 +5,18 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
     config = function()
       vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+
+      require("neo-tree").setup({
+        window = {
+          mappings = {
+            ["h"] = "", -- Unbind 'h'
+            ["j"] = "move_cursor_left", -- Remap 'j' to move left
+            ["k"] = "move_cursor_down", -- Remap 'k' to move down
+            ["l"] = "move_cursor_up", -- Remap 'l' to move up
+            [";"] = "move_cursor_right", -- Remap ';' to move right
+          },
+        },
+      })
     end,
   },
 }
