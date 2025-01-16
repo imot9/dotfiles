64dcd574c130ca5564f5144f6ad39aff5ee6ff98
Tamas Szamel <szamel.tamas9@gmail.com>
2025-01-16 17:24:46 +0100

    add swaync

--- .config/scripts/background.sh
+++ .config/scripts/background.sh
@@ -2,3 +2,6 @@
 wal -q -i "$1"
 source "$HOME/.cache/wal/colors.sh"
 killall -SIGUSR2 waybar
+sass ~/.config/swaync/style.scss ~/.config/swaync/style.css
+swaync-client -rs
+rm ~/.config/swaync/style.css.map
