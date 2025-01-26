50af367fb95b585124371897630ffc3097111a1e
Tamas Szamel <szamel.tamas9@gmail.com>
2025-01-26 07:14:18 +0100

    switch to pywal16 in nvim

--- .config/nvim/lua/plugins/pywal.lua
+++ .config/nvim/lua/plugins/pywal.lua
@@ -1,9 +1,10 @@
   return {
-   "AlphaTechnolog/pywal.nvim",
+   "uZer/pywal16.nvim",
     config = function()
-      require("pywal").setup({
+      require("pywal16").setup({
         theme = "default",
       })
+    vim.api.nvim_set_hl(0, "String", { fg = "#c19a6b"})
     end,
   }
 
