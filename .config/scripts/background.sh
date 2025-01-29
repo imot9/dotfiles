be5cc169867b3505a4261d96e2a6e80cff12f674
Tamas Szamel <szamel.tamas9@gmail.com>
2025-01-29 16:32:43 +0100

    always start with default wallpapers wal colors

--- .config/scripts/background.sh
+++ .config/scripts/background.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-wal -q -i "$1"
+
+if [ -z "$1" ]; then
+  wallpaper="~/.config/backgrounds/default.jpg"
+else
+  wallpaper="$1"
+  killall -SIGUSR2 waybar
+fi
+
+wal -q -i $wallpaper
 source "$HOME/.cache/wal/colors.sh"
-killall -SIGUSR2 waybar
 sass ~/.config/swaync/style.scss ~/.config/swaync/style.css
 swaync-client -rs
 rm ~/.config/swaync/style.css.map
