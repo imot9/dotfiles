0e7af2f88f3bf00b2c7533fa3b2b88c424024688
imot9 <szamel.tamas9@gmail.com>
2025-06-28 11:55:46 +0200

    fix: modify privileges in setup script, add missing modules

--- setup.sh
+++ setup.sh
@@ -1,51 +1,73 @@
 #!/bin/bash
 
 # shell
-pacman -S zsh
+sudo pacman -S zsh
 chsh -s $(which zsh)
 
 # yay
-pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && rm -rf yay
 
+python -m ensurepip --default-pip
+
 # basic packages
-pacman -S \
+sudo pacman -S \
   less \
   neovim \
+  vi \
   stow \
   ttf-firacode-nerd \
   ttf-font-awesome \
   kitty \
   lazygit \
   pamixer \
+  pavucontrol \
   brightnessctl \
   cliphist \
   fzf \
-  fastfetch
+  inxi \
+  fastfetch \
+  htop
 
 yay -S \
   oh-my-posh \
   dart-sass \
   waypaper \
   hyprshot \
-  pywalfox 
+  pywalfox \
+  python-pywal16
 
 # hypr and more
-pacman -S \
+sudo pacman -S \
   hyprland \
   hyprpaper \
   hyprlock \
+  hypridle \
   waybar \
   swaync \
-  rofi-wayland
+  rofi-wayland \
+  firefox
 
-# hide .desktop files in rofi
-for file in "/usr/share/applications/*.desktop"
-do
-  echo "NoDisplay=true >> $file";
+# list of files to keep in rofi app launcher
+exclude_files=(
+  "firefox.desktop"
+  "htop.desktop"
+  "kitty.desktop"
+  "nvim.desktop"
+  "nvidia-settings.desktop"
+  "waypaper.desktop"
+)
+
+for file in /usr/share/applications/*.desktop; do
+  file_name=$(basename "$file")
+  
+  if [[ ! " ${exclude_files[@]} " =~ " $file_name " ]]; then
+    if ! grep -q "NoDisplay=true" "$file"; then
+      echo "NoDisplay=true" >> "$file"
+      echo "Updated: $file"
+    fi
+  fi
 done
 
-# browser (last step not to remove the .desktop file)
-pacman -S firefox
