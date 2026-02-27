9dc416f9e46231ac5db84410db79bcb2b23b3091
Tamas Szamel <szamel.tamas9@gmail.com>
2026-02-27 19:05:40 +0100

    feat: specify sections in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,20 @@ return {
       options = {
         theme = "seoul256",
       },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+            shorting_target = 0,
+          },
+        },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      }
     })
   end,
 }
