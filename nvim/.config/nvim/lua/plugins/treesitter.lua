d1396cb45f4f40808c7b8ebdc28c9725a04cc0e2
Tamas Szamel <szamel.tamas9@gmail.com>
2026-05-03 10:06:14 +0200

    fix: treesitter changes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
   lazy = false,
   config = function()
-    local config = require("nvim-treesitter.configs")
+    local config = require("nvim-treesitter")
     config.setup({
       auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = false },
     })
   end,
 }
